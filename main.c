29ed40096016f1eba87939c491feb672382694cf
Byungkook Kim <bykim@c2r10s1.42seoul.kr>
2020-07-28 13:22:41 +0900

    put_image

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 typedef struct  s_vars {
     void        *mlx;
     void        *win;
+	void		*img;
 	int			color;
 	clock_t		start;
+	double		elapsed;
 
 }               t_vars;
 
@@ -45,30 +47,36 @@ int render_next_frame(t_vars *vars)
 	int rainbow[7][3] = {{255,000,000}, {255,102,000}, {255,255,000},
 {000,255,000},{000,000,255},{000,000,102},{127,000,255}};
 	
-	int s = (clock() - vars->start) / 100000;
-	int a = s % 7;
-	vars->color = create_trgb(0, rainbow[a][0], rainbow[a][1], rainbow[a][2]);
-	for (int i=0;i<250;i++) {
-		for (int j=0;j<10;j++) {
-			mlx_pixel_put(vars->mlx, vars->win, 125+i, 250+j, vars->color);
+	vars->elapsed = (double)(clock() -  vars->start) / CLOCKS_PER_SEC;
+	int a = (int)(5 * vars->elapsed);
+	vars->color = create_trgb(100 * a % 100, rainbow[a%7][0], rainbow[a%7][1], rainbow[a%7][2]);
+	for (int i=0;i<500;i++) {
+		for (int j=0;j<500;j++) {
+			mlx_pixel_put(vars->mlx, vars->win, 0+i, 0+j, vars->color);
 		}
 	}
+	//mlx_string_put(vars->mlx, vars->win, 200, 200, vars->color, "hyein");
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 125, 150);
 	return 0;
 }
 
 int main()
 {
 	t_vars vars;
+	int img_wid = 500;
+	int img_hei = 500;
 
 	vars.color = create_trgb(0,255,100,170);
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 500, 500, "new window");
 	vars.start = clock();
+	vars.img = mlx_xpm_file_to_image(vars.mlx, "./pengsu.xpm", &img_wid, &img_hei);
+
 
 	//mlx_key_hook(vars.win, keyboard, &vars);
 	//mlx_mouse_hook(vars.win, mouse, &vars);
-	mlx_hook(vars.win, 2, 1L<<0, keyboard, &vars);
-	mlx_hook(vars.win, 4, 1L<<2, mouse, &vars);
+	mlx_hook(vars.win, 2, 0, keyboard, &vars);
+	mlx_hook(vars.win, 4, 0, mouse, &vars);
 	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
 	mlx_loop(vars.mlx);
 }
